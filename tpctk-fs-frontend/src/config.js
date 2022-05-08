cade4d0738fe457c736e1c7c5b77a6f64d12afa0
milsman2 <milsman2@gmail.com>
2022-05-08 16:01:15 +0000

    working docker image

--- tpctk-fs-frontend/src/config.js
+++ tpctk-fs-frontend/src/config.js
@@ -2,7 +2,7 @@ import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 const env = runtimeEnv();
 const config = {
-  apiBasePath: env.REACT_APP_API_BASE_PATH || 'https://tpctk-code-server.kanebroslab.com/proxy/8000',
+  apiBasePath: env.REACT_APP_API_BASE_PATH || 'http://localhost:8000',
   reactAppMode: process.env.REACT_APP_MODE || 'dev',
 };
 
