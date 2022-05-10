8bd0a091d0d155462af87ebc48b48c652bcc4f29
milsman2 <milsman2@gmail.com>
2022-05-10 23:55:54 +0000

    trying out pool_pre_ping again

--- tpctk-fs-async-backend/app/db/session.py
+++ tpctk-fs-async-backend/app/db/session.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
 
-engine = create_async_engine(settings.async_database_url)
+engine = create_async_engine(settings.async_database_url, pool_pre_ping=True)
 
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
