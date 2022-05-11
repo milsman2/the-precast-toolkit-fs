19cd5f063dbcb2f2da58e63556fa8dcbbeedef70
milsman2 <milsman2@gmail.com>
2022-05-11 02:56:10 +0000

    updated docker image

--- tpctk-fs-frontend/src/config.js
+++ tpctk-fs-frontend/src/config.js
@@ -3,7 +3,7 @@ import runtimeEnv from '@mars/heroku-js-runtime-env';
 const env = runtimeEnv();
 const config = {
   apiBasePath: env.REACT_APP_API_BASE_PATH || 'https://fastapi.precasttoolkit.com',
-  reactAppMode: process.env.REACT_APP_MODE || 'dev',
+  reactAppMode: process.env.REACT_APP_MODE || 'production',
 };
 
 export default config;
